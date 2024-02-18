7a8616a894af84a7499a9a773c3858dc0c7c3494
dwhddnjs <per1215@naver.com>
2024-02-18 23:52:16 +0900

    new page 버튼 추가

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,18 +1,21 @@
 "use client"
 
 import { cn } from "@/lib/utils"
-import { ChevronLeft, ChevronsLeft, MenuIcon } from "lucide-react"
+import { ChevronLeft, ChevronsLeft, MenuIcon, PlusCircle } from "lucide-react"
 import { usePathname } from "next/navigation"
 import { ElementRef, useEffect, useRef, useState } from "react"
 import { useMediaQuery } from "usehooks-ts"
 import { UserItem } from "./user-item"
-import { useQuery } from "convex/react"
+import { useMutation, useQuery } from "convex/react"
 import { api } from "@/convex/_generated/api"
+import { Item } from "./item"
+import { toast } from "sonner"
 
 export const Navigation = () => {
   const pathname = usePathname()
   const isMobile = useMediaQuery("(max-width: 768px)")
   const documents = useQuery(api.documents.get)
+  const create = useMutation(api.documents.create)
 
   const isResizingRef = useRef(false)
   const sidebarRef = useRef<ElementRef<"aside">>(null)
@@ -92,6 +95,15 @@ export const Navigation = () => {
     }
   }
 
+  const handleCreate = () => {
+    const promise = create({ title: "Untitled" })
+    toast.promise(promise, {
+      loading: "Create a new note",
+      success: "New note created!",
+      error: "Failed to create a new note",
+    })
+  }
+
   return (
     <>
       <aside
@@ -114,6 +126,7 @@ export const Navigation = () => {
         </div>
         <div>
           <UserItem />
+          <Item onClick={handleCreate} label="New Page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
           {documents?.map((document) => (
