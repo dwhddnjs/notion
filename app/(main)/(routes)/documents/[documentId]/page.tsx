9922eaa5a485b47875b13e8a34575c31112be3c5
dwhddnjs <per1215@naver.com>
2024-02-24 23:49:07 +0900

    documentId page 추가

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,7 +1,32 @@
+"use client"
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel"
+import { getById } from "@/convex/documents"
+import { useQuery } from "convex/react"
 import React from "react"
 
-const DocumentIdPage = () => {
-  return <div className="">dsasdasdads</div>
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">
+  }
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents / getById, {
+    documentId: params.documentId,
+  })
+
+  if (document === undefined) {
+    return <div>Loading..</div>
+  }
+  ;``
+  return (
+    <div className="pb-40">
+      <div className="md:max-x-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initalData={document} />
+      </div>
+    </div>
+  )
 }
 
 export default DocumentIdPage
